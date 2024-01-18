4f07977f8ddbfef39f9fdfbd3e599244962c6f4e
ben <bb14738@ms.wasatch.edu>
2024-01-17 19:03:41 -0700

    finished orange

--- Assets/Scripts/Fruit/OrangeCode.cs
+++ Assets/Scripts/Fruit/OrangeCode.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using UnityEngine;
+
 
 public class OrangeCode : FruitCode
 {
+    public int splitAmount;
+    public GameObject splitObject;
     public override void Damage(int amount)
     {
         hp -= amount;
         if (hp < 1)
         {
             //split
+            for (int i = 0; i < splitAmount; i++)
+            {
+                GameObject o = Instantiate(splitObject);
+                Vector2 r = Random.insideUnitCircle*1;
+                Vector3 currentPos = transform.position;
+                o.transform.position = new Vector3(currentPos.x + r.x, currentPos.y, currentPos.z + r.y);
+                FruitCode f = o.GetComponent<FruitCode>();
+                f.pathNum = pathNum;
+                f.goalPos = goalPos;
+                StartButtonController.startButton.objects.Add(o);
+            }
+            StartButtonController.startButton.objects.Remove(gameObject);
             Destroy(gameObject);
         }
 
