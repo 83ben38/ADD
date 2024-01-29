490e1994670862f7dbb92c9e837de2263a4147d8
CAM <CM27938@ms.wasatch.edu>
2024-01-29 13:16:07 -0700

    blueberry

---
+++ Assets/Scripts/Fruit/BlueberryCode.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueberryCode : FruitCode
+{
+    public float speedIncrease;
+    public override void Damage(int amount)
+    {
+        hp -= amount;
+        if (hp < 1)
+        {
+            StartButtonController.startButton.objects.Remove(gameObject);
+            Destroy(gameObject);
+        }
+
+        float z = ((maxScale - minScale) * ((float)hp / maxHp))   + minScale;
+        transform.localScale = new Vector3(z, z, z);
+        speed += speedIncrease;
+    }
+}
