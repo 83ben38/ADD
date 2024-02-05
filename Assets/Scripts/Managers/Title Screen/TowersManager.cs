ac96a66f4276ada86aa07c1cf29f731d37a093b2
Ben <bb14738@ms.wasatch.edu>
2024-02-05 12:21:07 -0700

    Towers Scene

---
+++ Assets/Scripts/Managers/Title Screen/TowersManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowersManager : MonoBehaviour
+{
+    public GameObject cloneObject;
+    public Shape shape;
+    public ShapeScriptableObject SSO;
+    public static TowerController[] selected;
+
+    private void Start()
+    {
+        
+        shape = cloneObject.GetComponentInChildren<Shape>();
+        MapCreator.shape = shape;
+        shape.scriptableObject = SSO;
+        CreateMap();
+       
+    }
+
+    void CreateMap()
+    {
+        int[] towers = SaveData.save.getAvailableTowers();
+        for (int j = 0; j < towers.Length; j++)
+        {
+            Vector2 pos = shape.getPosition(new Vector2(j%5, 10-((j/5)+3)));
+            cloneObject.transform.position = new Vector3(pos.x,0,pos.y);
+            TowerController t = Instantiate(cloneObject).GetComponent<TowerController>();
+            t.state = new DescriptionState();
+            t.tower = TowerCodeFactory.getTowerCode(towers[j]);
+            t.tower.lvl = 1;
+            t.x = towers[j];
+            Vector3 scale = t.transform.localScale;
+            t.transform.localScale = new Vector3(scale.x, scale.y*2, scale.z);
+            t.towerVisual.updateTower();
+        }
+        Destroy(cloneObject);
+    }
+}
