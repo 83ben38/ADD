1f0d337a12021a3c28ae0effccf04c23a0f520ce
Mariteragi <jm18360@ms.wasatch.edu>
2024-02-08 13:44:50 -0700

    lemons and coconuts

---
+++ Assets/Scripts/Fruit/CoconutCode.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoconutCode : FruitCode
+{
+    [SerializeField]
+    private int reductionMod;
+    
+    public override void Damage(int amount)
+    {
+        hp -= amount / reductionMod;
+        if (hp < 1)
+        {
+            StartButtonController.startButton.objects.Remove(gameObject);
+            Destroy(gameObject);
+        }
+
+        float z = ((maxScale - minScale) * ((float)hp / maxHp))   + minScale;
+        transform.localScale = new Vector3(z, z, z);
+    }
+    
+}
