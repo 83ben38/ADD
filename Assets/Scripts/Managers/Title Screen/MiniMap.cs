25b2ccd31b47af009993a1f1d0e215ee8215c979
ben <bb14738@ms.wasatch.edu>
2024-02-10 11:24:06 -0700

    map selection fix

--- Assets/Scripts/Managers/Title Screen/MiniMap.cs
+++ Assets/Scripts/Managers/Title Screen/MiniMap.cs
@@ -6,11 +6,12 @@ public class MiniMap : MonoBehaviour
 {
     public void setUp(MapScriptableObject map)
     {
-        GameObject cloneObject = new GameObject("Tile",typeof(MeshCollider), typeof(MeshRenderer), typeof(MeshFilter));
-        cloneObject.AddComponent<Shape>();
-        Shape shape = cloneObject.GetComponent<Shape>();
+        GameObject cloneObject = new GameObject("Tile");
+        GameObject tileObject = new GameObject("Tile",typeof(MeshCollider), typeof(MeshRenderer), typeof(MeshFilter));
+        tileObject.transform.parent = cloneObject.transform;
+        tileObject.AddComponent<Shape>();
+        Shape shape = tileObject.GetComponent<Shape>();
         shape.scriptableObject = map.shape;
-        
         for (int i = 0; i < map.xDimensions; i++)
         {
             for (int j = 0; j < map.yDimensions; j++)
@@ -23,23 +24,23 @@ public class MiniMap : MonoBehaviour
                 clone.transform.localScale *= map.scaleAmt * 0.1f;
                 switch (map.map[i+(j*map.xDimensions)])
                 {
-                    case 0 : clone.GetComponent<Renderer>().material.color = ColorManager.manager.tile;
+                    case 0 : clone.GetComponentInChildren<Renderer>().material.color = ColorManager.manager.tile;
                         break;
-                    case 1 : clone.GetComponent<Renderer>().material.color = ColorManager.manager.wallPerm;
+                    case 1 : clone.GetComponentInChildren<Renderer>().material.color = ColorManager.manager.wallPerm;
                         break;
-                    case 2 : clone.GetComponent<Renderer>().material.color = ColorManager.manager.tilePerm;
+                    case 2 : clone.GetComponentInChildren<Renderer>().material.color = ColorManager.manager.tilePerm;
                         break;
-                    case 3 : clone.GetComponent<Renderer>().material.color = ColorManager.manager.portal1;
+                    case 3 : clone.GetComponentInChildren<Renderer>().material.color = ColorManager.manager.portal1;
                         break;
-                    case 4 : clone.GetComponent<Renderer>().material.color = ColorManager.manager.portal2;
+                    case 4 : clone.GetComponentInChildren<Renderer>().material.color = ColorManager.manager.portal2;
                         break;
-                    case 5 : clone.GetComponent<Renderer>().material.color = ColorManager.manager.portal3;
+                    case 5 : clone.GetComponentInChildren<Renderer>().material.color = ColorManager.manager.portal3;
                         break;
-                    case -1 : clone.GetComponent<Renderer>().material.color = ColorManager.manager.start;
+                    case -1 : clone.GetComponentInChildren<Renderer>().material.color = ColorManager.manager.start;
                         break;
-                    case -2 : clone.GetComponent<Renderer>().material.color = ColorManager.manager.end;
+                    case -2 : clone.GetComponentInChildren<Renderer>().material.color = ColorManager.manager.end;
                         break;
-                    case -3 : clone.GetComponent<Renderer>().material.color = ColorManager.manager.checkpoint;
+                    case -3 : clone.GetComponentInChildren<Renderer>().material.color = ColorManager.manager.checkpoint;
                         break;
                 }
             }
