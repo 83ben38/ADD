2c5372bc56824f90ba76adf32aa27fb289bf1b37
Ben <bb14738@ms.wasatch.edu>
2024-02-14 14:26:04 -0700

    freeplay fix

--- Assets/Scripts/Buttons/Game/FreeplayButton.cs
+++ Assets/Scripts/Buttons/Game/FreeplayButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,19 @@ using UnityEngine;
 public class FreeplayButton : Selectable
 {
     private Material _material;
+    private bool enabled = false;
     private void Start()
     {
         _material = GetComponent<Renderer>().material;
         _material.color = ColorManager.manager.tile;
+    }
+
+    private void OnEnable()
+    {
+        if (enabled)
+        {
+            gameObject.SetActive(false);
+        }
     }
 
     public override void MouseEnter()
@@ -23,6 +33,7 @@ public class FreeplayButton : Selectable
 
     public override void MouseClick()
     {
+        enabled = true;
         WinController.controller.gameObject.SetActive(false);
     }
 }
