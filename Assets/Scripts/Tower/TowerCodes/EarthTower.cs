f15343bd3329c839b590aae8968aaeae133d56a5
Ben <bb14738@ms.wasatch.edu>
2024-02-19 17:38:27 -0700

    earth upgrade 1

--- Assets/Scripts/Tower/TowerCodes/EarthTower.cs
+++ Assets/Scripts/Tower/TowerCodes/EarthTower.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class EarthTower : TowerCode
 {
-    private static Vector3[] directions = new[] {new Vector3(1,0,0),new Vector3(-1,0,0),new Vector3(0.5f,0,0.76f).normalized,new Vector3(0.5f,0,-0.76f).normalized,new Vector3(-0.5f,0,0.76f).normalized,new Vector3(-0.5f,0,-0.76f).normalized };
+    private static Vector3[] directions6 = new[] {new Vector3(1,0,0),new Vector3(-1,0,0),new Vector3(0.5f,0,0.76f).normalized,new Vector3(0.5f,0,-0.76f).normalized,new Vector3(-0.5f,0,0.76f).normalized,new Vector3(-0.5f,0,-0.76f).normalized };
+
+    private static Vector3[] directions4 = new[] { new Vector3(1, 0, 0), new Vector3(-1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, -1, 0) };
     public EarthTower(bool upgrade1, bool upgrade2, bool upgrade3) : base(upgrade1, upgrade2, upgrade3)
     {
         
@@ -12,7 +14,7 @@ public class EarthTower : TowerCode
 
     public override int getAttackSpeed()
     {
-        return lvl  > 4  ? 128 : 96;
+        return lvl  > 4  ? 64 : 48;
     }
 
     public override float getRange()
@@ -23,7 +25,7 @@ public class EarthTower : TowerCode
     public override bool shoot()
     {
         controller.towerVisual.shoot(rechargeTime);
-
+        Vector3[] directions = upgrade1 ? directions4 : directions6;
         foreach (var vector3 in directions)
         {
             GameObject projectile = Object.Instantiate(TowerCode.projectile);
