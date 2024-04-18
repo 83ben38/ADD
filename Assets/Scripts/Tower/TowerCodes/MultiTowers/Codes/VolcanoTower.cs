fe7c2f5491452d82e4a801cac7459e354409daa4
Ben <bb14738@ms.wasatch.edu>
2024-04-18 14:20:43 -0600

    volcano tower

--- Assets/Scripts/Tower/TowerCodes/MultiTowers/Codes/VolcanoTower.cs
+++ Assets/Scripts/Tower/TowerCodes/MultiTowers/Codes/VolcanoTower.cs
@@ -8,12 +8,15 @@ public class VolcanoTower : MultiTowerCode
 
     public VolcanoTower(bool upgrade1, bool upgrade2, bool upgrade3) : base(upgrade1, upgrade2, upgrade3)
     {
+        range = 3;
+        attackSpeed = 256;
     }
 
     public override void MouseClick(TowerController controller)
     {
         
     }
+    
 
     public override ProjectileCode create()
     {
@@ -23,5 +26,36 @@ public class VolcanoTower : MultiTowerCode
     public override Color getColor()
     {
         return ColorManager.manager.volcanoTower;
+    }
+    public override bool shoot()
+    {
+        List<Collider> sphere = new List<Collider>(Physics.OverlapSphere(self, getRange() * MapCreator.scale,LayerMask.GetMask("Enemy")));
+        if (sphere.Count > 0)
+        {
+            for (int i = 0; i < lvl * 4; i++)
+            {
+                if (sphere.Count == i)
+                {
+                    break;
+                }
+                FruitCode fc = sphere[i].gameObject.GetComponent<FruitCode>();
+                if (fc.hidden)
+                {
+                    sphere.RemoveAt(0);
+                    i--;
+                    continue;
+                }
+                GameObject projectile = Object.Instantiate(TowerCode.projectile);
+                ProjectileController pc = projectile.GetComponent<ProjectileController>();
+                pc.code = create();
+                pc.code.lvl = lvl;
+                pc.code.target = fc;
+                projectile.transform.position = controller.towerVisual.shoot(rechargeTime);
+                pc.material.color = getColor();
+                pc.code.Start(pc);
+            }
+            return true;
+        }
+        return false;
     }
 }
