4015ca5afaf71ee40f36314c2884b31dcee0f089
ben <bb14738@ms.wasatch.edu>
2024-09-02 12:17:33 -0600

    snow tower upgrade 3

---
+++ Assets/Scripts/Projectiles/SnowstormProjectile.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ProBuilder;
+using Math = System.Math;
+
+public class SnowstormProjectile : ProjectileCode
+{
+    public float radius;
+    public SnowstormProjectile(bool upgrade1, bool upgrade2, bool upgrade3) : base(upgrade1, upgrade2, upgrade3)
+    {
+        damage = 10;
+        pierce = 3;
+        radius = .5f + (lvl * 0.25f);
+        
+        if (upgrade1)
+        {
+            damage *= 2;
+        }
+
+        if (upgrade2)
+        {
+            radius /= 2;
+            damage /= 2;
+        }
+    }
+   
+    public override void tick(ProjectileController controller)
+    {
+        controller.transform.localScale = new Vector3(radius, radius, radius);
+        controller.transform.Translate(Time.deltaTime*move);
+        Collider[] hit = Physics.OverlapSphere(controller.transform.position, radius*MapCreator.scale, LayerMask.GetMask("Enemy"));
+        for (int i = 0; i < hit.Length; i++)
+        {
+            this.hit(hit[i].gameObject.GetComponent<FruitCode>(), controller);
+        }
+    }
+    public override int getDamage()
+    {
+        return base.getDamage()*2;
+    }
+    public virtual void hit(FruitCode fruit, ProjectileController controller)
+    {
+        if (pierced.Contains(fruit))
+        {
+            return;
+        }
+        pierced.Add(fruit);
+        ColorManager.manager.StartCoroutine(hitEnemy(fruit));
+        if (fruit.Equals(target))
+        {
+            target = null;
+        }
+
+        fruit.Damage(getDamage());
+        fruit.Damage(0);
+        fruit.Damage(0);
+    }
+
+    public override int getPierce()
+    {
+        return base.getPierce()*2;
+    }
+    IEnumerator hitEnemy(FruitCode fruit)
+    {
+        float z = fruit.speed/2;
+        fruit.speed -= z;
+        for (float i = 0; i < (upgrade1 ? 320f : 640f); i+=Time.deltaTime*64f)
+        {
+            yield return null;
+        }
+
+        fruit.speed += z;
+    }
+}
--- Assets/Scripts/Tower/TowerCodes/SnowTower.cs
+++ Assets/Scripts/Tower/TowerCodes/SnowTower.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 
 public class SnowTower : TowerCode
 {
-   
-
+    private bool snowStormUsed = false;
     public SnowTower(bool upgrade1, bool upgrade2, bool upgrade3) : base(upgrade1, upgrade2, upgrade3)
     {
         range = 0.5f;
         attackSpeed = 128;
+        if (upgrade3)
+        {
+            attackSpeed *= 3;
+            attackSpeed /= 2;
+        }
+
+        snowStormUsed = !upgrade3;
     }
 
     public override float getRange()
@@ -34,9 +42,54 @@ public class SnowTower : TowerCode
             }
         }
     }
+
+    public override void roundStart()
+    {
+        base.roundStart();
+        if (upgrade3)
+        {
+            snowStormUsed = false;
+        }
+    }
+
     public override void MouseClick(TowerController controller)
     {
-        
+        if (!snowStormUsed)
+        {
+            List<TowerController> tiles = PathfinderManager.manager.tiles;
+            float startX = tiles[0].transform.position.x;
+            float endX = tiles[^1].transform.position.x;
+            float startY = tiles[0].transform.position.z;
+            float endY = tiles[^1].transform.position.z;
+            bool side = true;
+            for (float i = startX; i < endX; i += (upgrade2 ? 0.25f : 0.5f))
+            {
+                GameObject projectile = Object.Instantiate(TowerCode.projectile);
+                ProjectileController pc = projectile.GetComponent<ProjectileController>();
+                pc.code = new SnowstormProjectile(upgrade1, upgrade2, upgrade3);
+                pc.code.lvl = lvl;
+                pc.code.move = new Vector3(0, 0, side ? -3 : 3);
+                projectile.transform.position = new Vector3(i, MapCreator.scale, side ? endY + 1 : startY - 1);
+                pc.material.color = getColor();
+                pc.code.Start(pc);
+                side = !side;
+            }
+
+            side = true;
+            for (float i = startY; i < endY; i += (upgrade2 ? 0.25f : 0.5f))
+            {
+                GameObject projectile = Object.Instantiate(TowerCode.projectile);
+                ProjectileController pc = projectile.GetComponent<ProjectileController>();
+                pc.code = new SnowstormProjectile(upgrade1, upgrade2, upgrade3);
+                pc.code.lvl = lvl;
+                pc.code.move = new Vector3(side ? -3 : 3, 0, 0);
+                projectile.transform.position = new Vector3(side ? endX + 1 : startX - 1, MapCreator.scale, i);
+                pc.material.color = getColor();
+                pc.code.Start(pc);
+                side = !side;
+            }
+            snowStormUsed = true;
+        }
     }
 
     public override ProjectileCode create()
